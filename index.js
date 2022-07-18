694db1eb096ddf6c21578ddc7ca486384152ff82
Mahefa Ny Anjara <hei.mahefa@gmail.com>
2022-07-18 21:30:02 +0300

    fix insert bugs

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
 import {faker} from '@faker-js/faker';
 import * as fs from 'fs';
+let base ="INSERT INTO account (first_name, last_name, nickname, birthday, gender, email, profile_pic) VALUES";
+let sum=""
 const templateFiller=(first_name,last_name,nickname,birthday,gender,email,profile_pic,)=>{
-    return (`('${first_name}','${last_name}','${nickname}','${birthday}','${gender}','${email}','${profile_pic}')`)
+    return (`${base} ('${first_name}','${last_name}','${nickname}','${birthday}','${gender}','${email}','${profile_pic}');`)
 };
-let total ="INSERT INTO account (first_name, last_name, nickname, birthday, gender, email, profile_pic) VALUES ";
 let limit = 250_000;
 for(let i = 0 ; i < limit; i++){
-    const first_name = faker.name.firstName();
-    const last_name =faker.name.lastName();
-    const nickname = faker.name.firstName();
+    const first_name = faker.name.firstName().replace("'","''");
+    const last_name =faker.name.lastName().replace("'","''");
+    const nickname = faker.name.firstName().replace("'","''");
     const birthday = faker.date.between('1990-01-01','2004-12-31').toISOString().split('T')[0]
     const gender = faker.name.gender(true)[0];
     const email = faker.internet.email();
     const profile_pic = faker.internet.url();
-    total+=templateFiller(first_name,last_name,nickname,birthday,gender,email,profile_pic)
-    total+= (i < limit -1 )?",":";"
+    sum+=templateFiller(first_name,last_name,nickname,birthday,gender,email,profile_pic)
 }
-fs.writeFileSync('data.sql',total);
-console.log(total.length);
+fs.writeFileSync('data.sql',sum);
+console.log(sum.length);
